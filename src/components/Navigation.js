d3636b8d35c89fbdeeaa48506bba8c50ca534378
Mari-8 <ryanaltimari8@gmail.com>
2021-07-29 23:18:00 -0500

    Added navigation bar component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,27 +5,14 @@ export default class App extends Component {
 
     render() {
         return (
-            // <div className="navBar">
-            //     <div className="logo">
-            //         <h1>RA.</h1>
-            //     </div>
-            //     <div className="center">
-            //         <div className="links">
-            //             <a href="/">Home</a>
-            //             <a href="/projects">Projects</a>
-            //             <a href="/about">About me</a>
-            //             <a href="/contact">Contact</a>
-            //         </div>
-            //     </div>
-            // </div>
-            <nav className="navbar navbar-expand-lg navbar-light ">
+            <nav className="navbar navbar-expand-lg navbar-dark"> 
                 <div className="container-fluid content-center">
-                  <a className="navbar-brand" href="/">RA.</a>
+                  <a className="navbar-brand" href="/">RA<span className="name-dot">.</span></a>
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                   </button>
                   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav text-center">
+                    <div className="navbar-nav d-md-flex d-block flex-row mx-md-auto mx-0">
                         <a className="nav-link" href="/">Home</a>
                         <a className="nav-link" href="/projects">Projects</a>
                         <a className="nav-link" href="/about">About Me</a>
