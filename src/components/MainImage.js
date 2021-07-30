93ea91535e937a8d9b5384b39ea8b4bc3b43e64b
Mari-8 <ryanaltimari8@gmail.com>
2021-07-29 23:16:42 -0500

    Added Main Image functional component

---
+++ src/components/MainImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainImage() {
+    return (
+        <span className="circle-image">
+            <img src="/images/FB07046C-ABD1-4DF8-8827-A86F59AD1985.JPG" className="img-main" alt="..."></img>
+        </span>
+    )
+}
