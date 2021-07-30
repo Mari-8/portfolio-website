0dd2b8851c4bc693e02c8e16a3a87c945e42fe2b
Mari-8 <ryanaltimari8@gmail.com>
2021-07-29 23:18:37 -0500

    Imported main image, and intro card components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import NameCard from '../components/NameCard'
+import MainImage from '../components/MainImage'
+import IntroCard from '../components/IntroCard'
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+          <Container>
+            <Grid 
+                container 
+                spacing={6}
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+            >
+                <Grid item xs>
+                    <NameCard /> 
+                </Grid>
+                <Grid item xs>
+                    <MainImage /> 
+                </Grid>
+                <Grid item xs>
+                    <IntroCard /> 
+                </Grid>
+            </Grid>
+          </Container>
         )
     }
 }
