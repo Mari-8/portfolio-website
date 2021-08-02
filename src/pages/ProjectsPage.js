871a1c97f47eaa19596e3c4065c77fe973dea0fe
Mari-8 <ryanaltimari8@gmail.com>
2021-08-01 19:03:51 -0500

    Added services and skils components

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Container from '@material-ui/core/Container';
+import ServicesChunk from '../components/ServicesChunk'
+import SkillLogos from '../components/SkillLogos'
 
 export default class ProjectsPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Projects page</h1>
-            </div>
+            <Container className="container-light" maxWidth="xl" disableGutters={true}>
+                <ServicesChunk /> 
+                <br /> <br /><br /> 
+                <SkillLogos />
+            </Container>
         )
     }
 }
