b94c2b4b38ff9a8000516002c87f46c2a7693942
Mari-8 <ryanaltimari8@gmail.com>
2021-08-04 12:15:13 -0500

    rendering services paragraph and logo components

---
+++ src/pages/SkillsPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Container from '@material-ui/core/Container';
+import ServicesChunk from '../components/ServicesChunk'
+import SkillLogos from '../components/SkillLogos'
+
+export default class SkillsPage extends Component {
+    render() {
+        return (
+            <Container className="container-light" maxWidth="xl" disableGutters={true}>
+                <ServicesChunk /> 
+                <br /> <br /><br /> 
+                <SkillLogos />
+            </Container>
+        )
+    }
+}
