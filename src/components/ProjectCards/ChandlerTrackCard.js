2b6dcc8e47e4c1616faf7f86a6eeac2a6719ea29
Mari-8 <ryanaltimari8@gmail.com>
2021-08-12 22:21:34 -0500

    Added chandler track card component

---
+++ src/components/ProjectCards/ChandlerTrackCard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    card: {
+        textAlign: 'center',
+        width: '500px',
+        height: '360px',
+        margin: '0 auto',
+        background: '#34343b',
+        color: '#d9d9d9',
+    }
+  });
+
+export default function ChandlerTrackCard(props) {
+    const styles = useStyles()
+
+    return (
+        <Card className={styles.card}>
+            <br />
+            <h1>ChandlerTrack</h1>
+            <CardContent>
+            <p>
+                A versatile candle making dashboard for any chandler to track recipes, inventory list, and notes. 
+            </p>
+            <ul className="no-bullet ">
+                <li>Ruby on rails</li>
+                <li>Pure JavaScript</li>
+                <li>HTML/CSS</li>
+                <li>Custom serializers</li>
+            </ul>
+            </CardContent>
+            <div>
+              <a href="https://github.com/Mari-8/ChandlerTrack/tree/master">
+                <button type="button" className="btn project-buttons">Code</button>
+              </a>
+                <button type="button" className="btn project-buttons mx-3" onClick={() => props.selectGal('chandler-track')}>Images</button>
+                <button type="button" className="btn project-buttons disabled">Demo</button>
+            </div>
+            <br />
+        </Card>
+    )
+}
