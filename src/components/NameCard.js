12fa80bcfdef1b5614ad9c766ca248731e02b0a7
Mari-8 <ryanaltimari8@gmail.com>
2021-08-22 20:47:51 -0500

    Changes to styling of name card

--- src/components/NameCard.js
+++ src/components/NameCard.js
@@ -1,70 +1,80 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
 
 const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-      marginTop: 100,
-      marginLeft: 100,
-      color: '#d9d9d9',
-    },
-    title: {
-      fontSize: 35,
-      color: '#d9d9d9', 
-      fontWeight: 'bold',
-    },
-    content: {
-        color: '#424249',
-        fontSize: 14,
-    },
-    button: {
-        color: 'white',
-    }
-  });
+  root: {
+    minWidth: 275,
+    marginTop: 100,
+    marginLeft: 150,
+    color: "#d9d9d9",
+  },
+  title: {
+    fontSize: 35,
+    color: "#d9d9d9",
+    fontWeight: "bold",
+  },
+  content: {
+    color: "#424249",
+    fontSize: 14,
+  },
+  button: {
+    color: "white",
+  },
+});
 
 export default function NameCard() {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <div>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          <h1>
+            Ryan
+            <br />
+            Altimari
+            <span className="name-dot">.</span>
+          </h1>
+        </Typography>
+        <br />
+        <div className="dash-left" />
+        <br />
+        <div className={classes.content}>
+          <a href="google.com" className="link-social">
+            <LinkedInIcon />
+          </a>
 
-    return (
-        <div className={classes.root}>
-            <div>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    <h1>
-                        Ryan 
-                        <br/> 
-                        Altimari
-                        <span className="name-dot">.</span>
-                    </h1>
-                </Typography>
-                <br/>
-                <div className="dash-left" /> 
-                <br/> 
-                <div className={classes.content}>
-                    <a href="google.com" className="link-social"><LinkedInIcon/></a> 
+          <a href="google.com" className="link-social">
+            <GitHubIcon className="marg-left" />
+          </a>
 
-                    <a href="google.com" className="link-social"><GitHubIcon className="marg-left"/></a> 
-                  
-                    <a href="google.com" className="link-social"><InstagramIcon className="marg-left"/></a> 
-                    
-                    <a href="google.com" className="link-social"><MenuBookIcon className="marg-left"/></a>
-                </div>
-                <br/>
-                <div>
-                    <Button color="blue" variant="outlined" size="large">
-                        Contact Me
-                    </Button>
-                </div>
-            </div>
-           
+          <a href="google.com" className="link-social">
+            <InstagramIcon className="marg-left" />
+          </a>
+
+          <a href="google.com" className="link-social">
+            <MenuBookIcon className="marg-left" />
+          </a>
         </div>
-    )
+        <br />
+        <div>
+          <Button color="blue" variant="outlined" size="large">
+            Contact Me
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
