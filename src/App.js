757eaa6a1d7fb3dd459e6be8a5727a6f56d3ed69
Mari-8 <ryanaltimari8@gmail.com>
2021-08-23 09:44:34 -0500

    Testing stacked pages instead of routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react'; 
-import './App.css';
-import HomePage from './pages/HomePage'
-import ContactPage from './pages/ContactPage'
-import ProjectsPage from './pages/ProjectsPage'
-import AboutPage from './pages/AboutPage'
-import Navigation from './components/Navigation'
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import SkillsPage from "./pages/SkillsPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import AboutPage from "./pages/AboutPage";
+import Navigation from "./components/Navigation";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Navigation /> 
-          <Route exact path='/' component={HomePage} /> 
+        <Navigation />
+        <HomePage />
+        <SkillsPage />
+        <ProjectsPage />
+        <AboutPage />
+        <ContactPage /> 
+        {/* <Route exact path='/' component={HomePage} /> 
           <Route exact path='/contact' component={ContactPage} />
           <Route exact path='/projects' component={ProjectsPage} /> 
-          <Route exact path='/about' component={AboutPage} /> 
+          <Route exact path='/about' component={AboutPage} />  
+        */}
       </Router>
     );
   }
