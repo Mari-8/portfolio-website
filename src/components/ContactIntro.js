9eb58b6cb06ed70c3717b3f6312b113aab9f9726
Mari-8 <ryanaltimari8@gmail.com>
2021-08-22 20:45:41 -0500

    added intro to contact page

---
+++ src/components/ContactIntro.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      fontSize: 25,
+      textAlign: 'center',
+      paddingTop: '150px',
+    },
+    blueColor: {
+        color: '#05639e'
+    },
+    textColor: {
+        color: '#d9d9d9',
+    }
+  });
+
+export default function ContactIntro() {
+
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <p className={classes.blueColor}>contact me</p>
+            <h2 className={classes.textColor}>How to connect with me</h2>
+            <br /> 
+            <div className="dash-center" />
+            <br /> 
+            <p className={classes.textColor}>
+                Here are my social accounts, feel free to reach out to me at any of these, <br/> I will respond as quickly as possible.
+            </p>
+        </div>
+    )
+}
